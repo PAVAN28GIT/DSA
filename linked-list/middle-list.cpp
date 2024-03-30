071da297a00e79aafde4dd17f733d8e5b5e10d59
PAVAN <107898628+PAVAN28GIT@users.noreply.github.com>
2024-03-30 16:54:17 +0530

    Create middle-list.cpp

---
+++ linked-list/middle-list.cpp
@@ -0,0 +1,21 @@
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int cnt = 0;
+        ListNode* temp = head;
+        while(temp!=NULL){
+            cnt++;
+            temp = temp->next;
+        }
+
+        cnt/=2;
+    
+        ListNode* tmp = head;
+        while(cnt!=0){
+            tmp = tmp->next;
+            cnt--;
+        }
+        return tmp;   
+    }
+};
