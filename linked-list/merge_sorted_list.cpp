988de7d3e6c143870f5b694afeac71397160c095
PAVAN <107898628+PAVAN28GIT@users.noreply.github.com>
2024-03-30 16:52:30 +0530

    Create merge_sorted_list.cpp

---
+++ linked-list/merge_sorted_list.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insert(ListNode* head,int val){
+        ListNode* node = new ListNode(val);
+        head->next = node;
+        head = node;
+        return head;
+    }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* head;
+
+        if (list1 == nullptr) {
+            return list2;
+            }
+        if (list2 == nullptr) {
+            return list1;
+        }
+
+        if(list1->val <= list2->val){
+            head = new ListNode(list1->val);
+            list1 = list1->next;
+        }else{
+            head = new ListNode(list2->val);
+            list2 = list2->next;
+        }
+        ListNode* temp = head;
+        while(list1 != NULL && list2 != NULL){
+            if(list1->val <= list2->val){
+                temp = insert(temp,list1->val);
+                list1 = list1->next;
+            }
+            else {
+                temp = insert(temp,list2->val);
+                list2 = list2->next;
+            }
+        }
+        while(list1!= NULL){
+            temp = insert(temp,list1->val);
+            list1 = list1->next; 
+        }
+        while(list2!= NULL){
+            temp = insert(temp,list2->val);
+            list2 = list2->next; 
+        }
+        
+        return head;
+    }
+};
+
