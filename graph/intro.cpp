9f0c1eec94a6ec7001ddc76874e6fbf6d6a619cb
PAVAN <107898628+PAVAN28GIT@users.noreply.github.com>
2024-06-09 00:10:58 +0530

    Create intro.cpp

---
+++ graph/intro.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// MATRIX
+class graph
+{
+public:
+    void adj_matrix()
+    {
+        int n; //  no of nodes
+        int m; // no of edges
+
+        cin >> n >> m;
+        int adj[n + 1][n + 1];
+        for (int i = 0; i < m; i++)
+        {
+            int u, v;
+            cin >> u >> v;
+            adj[u][v] = 1;
+            adj[v][u] = 1;
+        }
+    }
+
+    void adj_list()
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> list(n + 1);
+        for (int i = 0; i < m; i++)
+        {
+            int u, v;
+            cin >> u >> v;
+            list[u].push_back(v);
+            list[v].push_back(u);
+        }
+    }
+};
+
+int main()
+{
+
+    graph g1;
+    g1.adj_list();
+    g1.adj_matrix();
+
+    return 0;
+}
