25a00bbe8307210719a1bc9626f4a3b058e17fea
PAVAN <107898628+PAVAN28GIT@users.noreply.github.com>
2024-07-11 17:22:55 +0530

    Update 3sum.cpp

--- arrays/3sum.cpp
+++ arrays/3sum.cpp
@@ -1,10 +1,46 @@
-//problem : 
+//problem : https://leetcode.com/problems/3sum/
 
+
+// 2 pointer solution
 
 class Solution {
 public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+  
+        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
 
-// topics :  hashset 
+
+        for( int i=0 ;i<nums.size();i++){
+            if(i>0 && nums[i]==nums[i-1]){ // to avoid duplicate 
+                continue;
+            }
+            int j = i+1;
+            int k = nums.size()-1;
+            while(j<k){
+                if(nums[i]+nums[j]+nums[k] == 0){
+                    result.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    while(nums[j]==nums[j-1] && j<k){ // to avoid duplicates
+                        j++;
+                    }
+                }
+                else if (nums[i]+nums[j]+nums[k]<0){         
+                    j++;              
+                }
+                else{
+                    k--;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+//   hashset solution : TLE
+
+class Solution {
+public:
 
     vector<vector<int>> threeSum(vector<int>& nums) {
         
