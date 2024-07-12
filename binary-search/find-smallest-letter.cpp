8fa0afea4507e146c35c7d683d0da5bbff62f8a6
PAVAN <107898628+PAVAN28GIT@users.noreply.github.com>
2024-07-12 20:40:10 +0530

    Update find-smallest-letter.cpp

--- binary-search/find-smallest-letter.cpp
+++ binary-search/find-smallest-letter.cpp
@@ -37,3 +37,32 @@ public:
     }
 
 };
+// BINARY SEARCH METHOD 2ND TIME
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int l = 0;
+        int r = letters.size()-1;
+
+        if(letters[r]<=target){
+            return letters[0];
+        }
+        while(l<r){
+            int mid = l+(r-l)/2;
+            if( letters[mid] == target){
+                while( mid+1 < letters.size() && letters[mid+1]==letters[mid]){
+                    mid++;
+                }
+                return letters[mid+1];
+            }
+            else if(letters[mid] >target){
+                r = mid;
+            }
+            else if (letters[mid]<target){
+                l = mid+1;
+            }
+        }
+        return letters[r];
+    }
+};
