7c0c06808db4b25616a49b3e201e2053123a60e0
PAVAN <107898628+PAVAN28GIT@users.noreply.github.com>
2024-07-20 14:54:35 +0530

    Create trees.cpp

---
+++ concepts/trees.cpp
@@ -0,0 +1,17 @@
+// binary tree : each node has max 2 children
+
+// types :
+
+// 1. full bt : each node has either 0 or 2
+
+// 2. complete bt : all levels are completely filled except for the last level.. and in the last level all nodes shld be towards left only..
+
+// 3. perfect bt : all leaf nodes are at same level
+
+// 4. balanced bt : height of tree at max log2 (N) , n is no of nodes. ------> ( height(left) - height(right) <=1)
+
+// 5. degenerate bt : stright tree like linked list
+
+
+
+  
