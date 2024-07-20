1e3128a49d9fd123a4c4d9abfe09704b357c768e
PAVAN <107898628+PAVAN28GIT@users.noreply.github.com>
2024-07-20 16:16:39 +0530

    Create maxpathsum.cpp

---
+++ trees/maxpathsum.cpp
@@ -0,0 +1,29 @@
+// problem :
+
+// solution :  https://www.youtube.com/watch?v=WszrfSwMz58&list=PLkjdNRgDmcc0Pom5erUBU4ZayeU9AyRRu&index=18
+// CRAZY PROBLEM...... 
+
+
+class Solution {
+public:
+    int maxpath(TreeNode* root, int& maxi){
+        if(root == NULL){
+            return 0;
+        }
+        int left = maxpath(root->left,maxi);
+        int right = maxpath(root->right,maxi);
+
+        if (left < 0) left=0;
+        if (right < 0) right =0;
+        maxi = max(maxi, ( left+right+root->val) );
+        return max(left,right)+root->val; 
+
+    }
+    int maxPathSum(TreeNode* root) {
+
+        int maxi = INT_MIN;
+        maxpath(root, maxi);
+        return maxi;
+              
+    }
+};
