b67a89274f73a8a32087291ad166de5f76ebfcdf
PAVAN <107898628+PAVAN28GIT@users.noreply.github.com>
2024-07-25 21:39:16 +0530

    Update merge_sorted_list.cpp

--- linked-list/merge_sorted_list.cpp
+++ linked-list/merge_sorted_list.cpp
@@ -1,6 +1,48 @@
 // problem link : https://leetcode.com/problems/merge-two-sorted-lists/
 
+// inplace 
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL )return list2;
+        if(list2 == NULL) return list1;
+        ListNode* dummy = new ListNode();     
+        ListNode* main;  
+        ListNode* l2;        
+        if(list1->val <= list2->val){
+            main = list1;
+            l2 = list2;
+        }
+        else{
+            main = list2;
+            l2 = list1;
+        }
+        dummy->next = main;
+        ListNode * temp;
+        ListNode * prev;
+        while(l2!=NULL){
+            if(main == NULL){
+                prev->next = l2;
+                break;
+            }
+            if(main->val<=l2->val){
+                prev = main;
+                main = main->next;               
+            }
+            else{
+                temp = l2->next;
+                prev->next = l2;
+                prev = l2;
+                l2->next = main;
+                l2 = temp;
+            }
+            
+        }
+    return dummy->next;
+    }
+};
 
+// external list
 class Solution {
 public:
     ListNode* insert(ListNode* head,int val){
