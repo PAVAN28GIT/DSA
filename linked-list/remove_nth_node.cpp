5f208fddd726ca3fba6a652b17fc6a5778adb43a
PAVAN <107898628+PAVAN28GIT@users.noreply.github.com>
2024-07-25 23:13:07 +0530

    Update remove_nth_node.cpp

--- linked-list/remove_nth_node.cpp
+++ linked-list/remove_nth_node.cpp
@@ -1,5 +1,30 @@
 // problem link : https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
 
+// rabit- tortoise method
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int i = 0;
+        ListNode* dummy = new ListNode();
+        dummy->next = head;
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+
+        while(i<n){
+            fast = fast -> next;
+            i++;
+        }
+        while(fast->next != NULL){
+            fast = fast->next;
+            slow = slow->next;
+        }
+        slow->next = slow->next->next;
+        return dummy->next;
+    }
+};
+
+// brute force
 
 class Solution {
 public:
