186d838a18d2aa0384bbf721ddd3341f7e84fb77
PAVAN <107898628+PAVAN28GIT@users.noreply.github.com>
2024-07-25 19:35:49 +0530

    Update middle-list.cpp

--- linked-list/middle-list.cpp
+++ linked-list/middle-list.cpp
@@ -1,4 +1,23 @@
 //problem link : https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+
+   // tortoise and rabbit method
+class Solution {
+public:
+ 
+    ListNode* middleNode(ListNode* head) {
+         ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast->next!=NULL){
+            fast = fast->next;
+            slow = slow->next;
+            if(fast->next != NULL)fast = fast->next;
+        }
+        return slow;
+    }
+};
+
+// brute force 
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
